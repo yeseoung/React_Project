67a6c3055bb8799695d2dfb885376d0c3094cb7e
고예성 <kys030925@naver.com>
2026-05-19 13:42:12 +0900

    원상복구함 딱히 변경 ㄴㄴ

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <div className="Header">
-      <h3>깃 테스트</h3>
+      <h3>오늘은! 📆</h3>
       <h1>{new Date().toDateString()}</h1>
     </div>
   );
